c75bc0bcb98d73c8b0fade2936dab469cdfa2455
alan <alan@offspringdigital.com>
2024-08-26 16:33:08 +0700

    Changes

--- android/app/src/main/java/com/alan/imode/MainApplication.kt
+++ android/app/src/main/java/com/alan/imode/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.demmoapp
+package com.alan.imode
 
 import android.app.Application
 import com.facebook.react.PackageList
--- ios/iModeTests/iModeTests.m
+++ ios/iModeTests/iModeTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface demmoappTests : XCTestCase
+@interface iModeTests : XCTestCase
 
 @end
 
-@implementation demmoappTests
+@implementation iModeTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
